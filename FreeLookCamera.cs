3a6623989e2304a65e361e340f23ee276b050cfe
D0ct0rDave <dementedworld@gmail.com>
2024-05-23 20:30:07 +0200

    adding main files

---
+++ FreeLookCamera.cs
@@ -0,0 +1,76 @@
+
+using System;
+using Godot;
+
+partial class FreeLookCamera : FreeLookCameraBase
+{	
+	public bool Enabled
+	{
+		set
+        {
+            m_enabled = value;
+            if (m_enabled)
+            {
+                // save current active camera
+			    m_previousCamera = GetViewport().GetCamera3D();
+                m_previousMouseMode = Input.MouseMode;
+
+                if (m_previousCamera != null)
+                {
+                    // Copy current camera properties into this camera
+                    GlobalTransform = m_previousCamera.GlobalTransform;
+                    Fov = m_previousCamera.Fov;
+                    Near = m_previousCamera.Near;
+                    Far = m_previousCamera.Far;
+                    Projection = m_previousCamera.Projection;
+
+                    // disable current camera
+                    m_previousCamera.Current = false;
+                }
+
+                // Enable free look camera and set as the current one
+                Current = true;
+            }
+            else
+            {
+                
+                Current = false;
+                if (m_previousCamera != null)
+                {
+			        m_previousCamera.Current = true;                
+    			    Input.MouseMode = m_previousMouseMode;
+                }
+            }
+        }
+
+		get
+		{
+			return m_enabled;
+		}
+    }
+
+    private bool m_enabled = false;
+	private Camera3D m_previousCamera;
+    private Input.MouseModeEnum m_previousMouseMode;
+    public override void _Ready()
+    {
+        Current = false;
+    }
+    public override void _Input(InputEvent _event)
+    {
+        if (! Enabled)
+        {
+            return;
+        }
+        base._Input(_event);
+    }
+    public override void _Process(double delta)
+    {
+        if (! Enabled)
+        {
+            return;
+        }
+
+        base._Process(delta);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# SimpleFreeLookCamera4CSharp
-A c# implementation of the SimpleFreeLookCamera4 found in the AssetStore 
+# Simple Free-Look Camera
+A c# implementation of the SimpleFreeLookCamera4 found in the AssetStore:
+ 
+A simple 3D 100-line free-look camera for Godot 4.0 that mimics the functionality of the editor's camera for in-game use.
+
+Ported from [Richardhyy's Godot 4.x version](https://github.com/richardhyy/simple-free-look-camera-4).
+who ported from [Adamviola's Godot 3.2 version](https://github.com/adamviola/simple-free-look-camera).
+
+## Usage
+Use **W** and **S** to move forward and backward.
+
+Use **A** and **D** to move left and right.
+
+Use **Q** and **E** to move up and down.
+
+Roll the **scroll wheel** to increase and decrease movement speed.
+
+Press **shift** to move faster, and hold **alt** to move slower.
+
+Hold down the **right mouse button** to rotate the camera.
+There's a slider in the editor to control mouse sensitivity.
+
+## Installation
+Attach `FreeLookCameraBase.cs` or `FreeLookCamera.cs` to your Camera3D node. That's it!
+
+- Attaching FreeLookCameraBase behaves exactly like the original GD script versions.
+
+- If you attach FreeLookCamera.cs instead, you'll be able to switch from your current game camera's to the free one, just setting the **Enabled** property of it. For instance by handling an input action from your script:
+
+```c++
+...
+if (Input.IsActionJustReleased("ToggleFreeCameraAction"))
+{
+    m_freeCameraNode.Enabled = !m_freeCameraNode.Enabled;
+}
+...
+```
+
+## License
+[MIT](https://opensource.org/licenses/MIT).
+ 
